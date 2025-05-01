f94984416af1d7e029066944c41d102a68b39bb0
leonlak <leonardoramirez2906@hotmail.com>
2025-05-01 10:15:19 -0500

    Version 2

--- admin/propiedades/actualizar.php
+++ admin/propiedades/actualizar.php
@@ -1,6 +1,6 @@
 <?php
 require '../../includes/funciones.php';
-//$auth = estaAutenticado();
+$auth = estaAutenticado();
 
 
 if(!$auth) {
@@ -22,7 +22,7 @@ require '../../includes/config/database.php';
 $db = conectarDB();
 
 //Obtener los datos de la propiedad
-$consulta = "SELECT * FROM services_web.servicios WHERE id = $id";
+$consulta = "SELECT * FROM bienesraices_crud.propiedades WHERE id = $id";
 $resultado = mysqli_query($db, $consulta);
 $propiedad = mysqli_fetch_assoc($resultado);
 
@@ -30,34 +30,42 @@ $propiedad = mysqli_fetch_assoc($resultado);
 
 
 //Consulta para obtener los vendedores
-$consulta = "SELECT * FROM services_web.desarrollador";
+$consulta = "SELECT * FROM bienesraices_crud.vendedores";
 $resultado = mysqli_query($db, $consulta);
 
 
 //Arreglo con mensajes de errores
 $errores = [];
 
-$nombre = $servicio['nombre'];
-$precio = $servicio['precio'];
-$descripcion = $servicio['descripcion'];
-$dificultad = $servicio['dificultad'];
-$fechai = $servicio['fechai'];
-$fechaf = $servicio['fechaf'];
-$desarrolladorId = $servicio['desarrollador_Id'];
-$imagenServicio = $servicio['imagen'];
+$titulo = $propiedad['titulo'];
+$precio = $propiedad['precio'];
+$descripcion = $propiedad['descripcion'];
+$habitaciones = $propiedad['habitaciones'];
+$wc = $propiedad['wc'];
+$estacionamiento = $propiedad['estacionamiento'];
+$vendedorId = $propiedad['vendedor_Id'];
+$imagenPropiedad = $propiedad['imagen'];
 
 //Ejecutar el codigo despues de que el usuario envia el formulario
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
+    //echo "<pre>";
+    //var_dump($_POST);
+    //echo "</pre>";
 
+    //echo "<pre>";
+    //var_dump($_FILES);
+    //echo "</pre>";
 
-$nombre = mysqli_real_escape_string( $db, $_POST['nombre']);
+    //exit;
+
+$titulo = mysqli_real_escape_string( $db, $_POST['titulo']);
 $precio = mysqli_real_escape_string ($db, $_POST['precio']);
 $descripcion = mysqli_real_escape_string( $db, $_POST['descripcion']);
-$dificultad = mysqli_real_escape_string($db, $_POST['dificultad']);
-$fechai = mysqli_real_escape_string($db, $_POST['fechai']);
-$fechaf = mysqli_real_escape_string($db, $_POST['fechaf']);
-$desarrolladorId = mysqli_real_escape_string($db, $_POST['desarrollador']);
+$habitaciones = mysqli_real_escape_string($db, $_POST['habitaciones']);
+$wc = mysqli_real_escape_string($db, $_POST['wc']);
+$estacionamiento = mysqli_real_escape_string($db, $_POST['estacionamiento']);
+$vendedorId = mysqli_real_escape_string($db, $_POST['vendedor']);
 $creado = date('y/m/d');
 
 //Asignar files hacia una variable
@@ -66,7 +74,7 @@ $imagen = $_FILES['imagen'];
 
 
 
-if (!$nombre) {
+if (!$titulo) {
     $errores[] = "Debes añadir un titulo";
 
 }
@@ -80,24 +88,24 @@ if (strlen($descripcion) < 50) {
 
 }
 
-if (!$dificultad) {
-    $errores[] = "El numero de dificultad es obligatorio";
+if (!$habitaciones) {
+    $errores[] = "El numero de habitaciones es obligatorio";
 
 }
 
-if (!$fechai) {
-    $errores[] = "la fecha de inicio es obligatorio";
+if (!$wc) {
+    $errores[] = "El numero de baños es obligatorio";
 
 }
 
-if (!$fechaf) {
-    $errores[] = "la fecha final es obligatorio";
+if (!$estacionamiento) {
+    $errores[] = "El numero de lugares de Estacionamiento es obligatorio";
 
 
 }
 
-if (!$desarrolladorId) {
-    $errores[] = "Elige un desarrollador";
+if (!$vendedorId) {
+    $errores[] = "Elige un vendedor";
 
 }
 
@@ -108,7 +116,9 @@ if ($imagen['size'] > $medida) {
     $errores[] = 'La imagen es muy pesada';
 }
 
-
+//echo "<pre>";
+//var_dump($errores);
+//echo "</pre>";
 
 //Revisar que el array de errores este vacio
 if (empty($errores)) {
@@ -144,8 +154,8 @@ move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . $nombreImagen );
 
 
 // insertar en la base de datos
-$query = "UPDATE `services_web`.`servicios` SET `nombre` = '$nombre', `precio` = '$precio', `imagen` = '$nombreImagen', `descripcion` = '$descripcion', 
-`dificultad` = '$dificultad', `fechai` = '$fechai', `fechaf` = '$fechaf', `desarrollador_Id` = '$desarrolladorId' WHERE id = $id ";
+$query = "UPDATE `bienesraices_crud`.`propiedades` SET `titulo` = '$titulo', `precio` = '$precio', `imagen` = '$nombreImagen', `descripcion` = '$descripcion', 
+`habitaciones` = '$habitaciones', `wc` = '$wc', `estacionamiento` = '$estacionamiento', `vendedor_Id` = '$vendedorId' WHERE id = $id ";
 
 
 
@@ -169,7 +179,7 @@ if($resultado) {
 
 
 
- //incluirTemplate('header');
+ incluirTemplate('header');
  ?>
 
     <main class="contenedor seccion">
@@ -205,34 +215,34 @@ if($resultado) {
             </fieldset>
 
             <fieldset>
-                <legend>Informacion Servicios Web</legend>
+                <legend>Informacion Propiedad</legend>
 
-                <label for="dificultad">Dificultad:</label>
-                <input type="number" id="dificultad" name="dificultad" placeholder="Ej: 3" min="1" max="9" value="<?php echo $dificultad; ?>">
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" value="<?php echo $habitaciones; ?>">
 
-                <label for="fechai">Fecha de Inicio:</label>
-                <input type="date" id="fechai" name="fechai" placeholder="Ej: 3" min="1" max="9" value="<?php echo $fechai; ?>">
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value="<?php echo $wc; ?>">
 
-                <label for="estacionamiento">Fecha final:</label>
-                <input type="date" id="fechaf" name="fechaf" placeholder="Ej: 3" min="1" max="9" value="<?php echo $fechaf; ?>">
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">
             </fieldset>
 
             <fieldset>
-                <legend>Desarrolador</legend>
+                <legend>Vendedor</legend>
 
-                <select name="desarrollador">
+                <select name="vendedor">
                     <option value="">--Seleccione --</option>
-                   <?php while($desarrollador = mysqli_fetch_assoc($resultado)): ?>
-                    <option <?php echo $desarrolladorId === $desarrollador['id'] ? 'selected' : ''; ?>  value="<?php echo $desarrollador['id']; ?>"><?php echo $desarrollador['nombre'] . " " . $desarrollador['apellido']; ?></option>
+                   <?php while($vendedor = mysqli_fetch_assoc($resultado)): ?>
+                    <option <?php echo $vendedorId === $vendedor['id'] ? 'selected' : ''; ?>  value="<?php echo $vendedor['id']; ?>"><?php echo $vendedor['nombre'] . " " . $vendedor['apellido']; ?></option>
 
                    <?php endwhile; ?>
                 </select>
             </fieldset>
-            <input type="submit" value="Actualizar Servicio"  class="boton boton-verde">
+            <input type="submit" value="Actualizar Propiedad"  class="boton boton-verde">
         </form>
     </main>
 
 <?php
 
-//incluirTemplate('footer');
+incluirTemplate('footer');
  ?>
--- includes/config/database.php
+++ includes/config/database.php
@@ -1,14 +1,12 @@
-<?php 
+<?php
 
-function conectarDB() : mysqli {
-    $db = new mysqli('localhost', 'root', 'admin', 'services_web');
-
+function conectarDB() : mysqli{
+    $db = mysqli_connect('localhost', 'root', 'admin', 'bienesraices_crud');
+    
 
     if(!$db) {
         echo "Error no se pudo conectar";
         exit;
-    } 
-
+    }
     return $db;
-    
 }
